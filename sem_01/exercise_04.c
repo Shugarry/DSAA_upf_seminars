4ae5d2e33004b66953adff71048d777c5d50e04a
frey-gal <frey-gal@student.42barcelona.com>
2025-01-20 20:21:39 +0100

    trying stuff out for 04

--- sem_01/exercise_04.c
+++ sem_01/exercise_04.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-typedef struct
+typedef struct //player type object with name, left and right hand values.
 {
 	int		left;
 	int		right;
@@ -9,5 +9,17 @@ typedef struct
 
 int main()
 {
-	
+	player  alice = {1, 1, Alice};
+    player  bob = {1, 1, Bob};
+    int     turns;
+    char    turn_doer[3];
+
+
+    printf("Please input amount of turns: ");
+    scanf("%d\n", &turns);
+    
+    for (int i = 0; i < turns; i++)
+    {
+        scanf("%c %c %c\n", turn_doer[0], turn_doer[1], turn_doer[2]);
+    }
 }
