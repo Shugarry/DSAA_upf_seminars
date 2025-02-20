9da5a48ebefed57320c8b7a8e312ef76ce6cd869
frey-gal <frey-gal@student.42barcelona.com>
2025-02-20 18:10:15 +0100

    finished txt parsing, commencing array manipulation, boarding / moving / serving...

--- sem_03/s3_u252301.c
+++ sem_03/s3_u252301.c
@@ -15,27 +15,21 @@ typedef struct
 	Client	*clients;
 }	Elevator;
 
-Client	*append_client(Client *list, Client *new_client, int num_clients)
+Client	*append_client(Client *list, Client new_client, int num_clients)
 {
 	Client	*tmp;
-
-	if (num_clients == 1)
-	{
-		tmp = new_client;
-		return (tmp);
-	}
 
 	tmp = (Client *)malloc(num_clients * sizeof(Client));
 	if (!tmp)
 	{
-		printf("malloc failure");
+		printf("malloc failure\n");
 		exit(EXIT_FAILURE);
 	}
 
-	for (int i = 0; i < num_clients - 2; i++)
+	for (int i = 0; i < num_clients - 1; i++)
 		tmp[i] = list[i];
 	free(list);
-	tmp[num_clients - 1] = *new_client;
+	tmp[num_clients - 1] = new_client;
 	return (tmp);
 }
 
@@ -51,30 +45,106 @@ Client	*read_clients(int *num_clients)
 		printf("Error opening file\n");
 		exit(EXIT_FAILURE);
 	}
-	while (fscanf(file, " %s %d %d", tmp.name, &tmp.org, &tmp.dst) == 3);
+	while (fscanf(file, "%s %d %d ", tmp.name, &tmp.org, &tmp.dst) == 3)
 	{
-		/*
 		if (!tmp.name[0] || tmp.org == -1 || tmp.dst == -1)
 		{
 			printf("Parsing error, please make sure that your clients.txt file is correct\n");
 			exit(EXIT_FAILURE);
 		}
-		*/
 		(*num_clients)++;
-		list = append_client(list, &tmp, *num_clients);
+		list = append_client(list, tmp, *num_clients);
 		tmp.name[0] = '\0';
 		tmp.org = -1;
 		tmp.dst = -1;
 	}
+	return (list);
 }
 
-int main()
+void	move(Elevator *elevator, int num_waiting_clients, Client *waiting_clients)
 {
-	Elevator elevator = {0, 0, NULL};
 
-	elevator.clients = read_clients(&elevator.num_clients);
-	for (int i = 0; i < elevator.num_clients; i++)
+}
+
+void	order_org(Client **c, int waiting)
+{
+	Client	tmp;
+	Client	*clients = *c;
+	int		swapped = 0;
+
+	for (int i = 0; i < waiting - 1; i++)
 	{
-		printf("client %d -> name: %s, org: %d, dst: %d\n", i, elevator.clients[i].name, elevator.clients[i].org, elevator.clients[i].org);
+		for (int j = 0; j < waiting - 1 - i; j++)
+		{
+			if (clients[j].org > clients[j + 1].org)
+			{
+				tmp = clients[j];
+				clients[j] = clients[j + 1];
+				clients[j + 1] = tmp;
+				swapped = 1;
+			}
+		}
+		if (swapped)
+			break ;
+	}
+}
+
+void	order_dst(Client **c, int in_elevator)
+{
+	Client	tmp;
+	Client	*clients = *c;
+	int		diff_a, diff_b;
+	int		swapped = 0;
+
+	for (int i = 0; i < in_elevator - 1; i++)
+	{
+		for (int j = 0; j < in_elevator - 1 - i; j++)
+		{
+			diff_a = abs(floor - clients[j].dst);
+			diff_b = abs(floor - clients[j + 1].dst);
+			if (diff_a > diff_b)
+			{
+				tmp = clients[j];
+				clients[j] = clients[j + 1];
+				clients[j + 1] = tmp;
+				swapped = 1;
+			}
+		}
+		if (swapped)
+			break ;
+	}
+}
+
+void	board(Elevator *elevator, Client **w_c, int *waiting)
+{
+	Client	*waiting_clients = w_c;
+	Client	*tmp
+	int		boarding = 0;
+
+	while (elevator->floor == waiting_clients[boarding].org)
+		boarding++;
+	*waiting -= boarding;
+	tmp = (Clients *)malloc(sizeof(Clients) * (elevator->num_clients + boarding));
+	for (int i = 0; i < elevator->num_clients; i++)
+	{
+		tmp[i] = elevator->clients[i];
+	}
+}
+
+int	main(void)
+{
+	Elevator	elevator = {0, 0, NULL};
+	int			num_waiting;
+	Client		*waiting_clients = read_clients(num_waiting);
+
+	while(waiting_clients && elevator.clients)
+	{
+		order_org(&waiting_clients, num_waiting);
+		if (elevator.floor == waiting_clients[0].org)
+			board(&elevator, &waiting_clients);
+		order_dst(&elevator->clients, elevator.num_clients);
+		if (elevator.floor == elevator->clients.dst)
+			serve(&elevator, waiting_clients);
+		move();
 	}
 }
